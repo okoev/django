f7bd68e9bd1c5c37a5b0f167f5e99c8494683f5f
okoev <syjmykbaktybekov826@example.com>
2024-01-11 12:18:44 +0600

    First commit

---
+++ django_movie/movies/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
+
+from .models import Movie, Category, Actor, Genre
+from .forms import ReviewForm
+
+
+class GenreYear():
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.ojects.fiter(draft=False).values("year")
+
+
+class MoviesView(GenreYear, ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+class MovieDetailView(GenreYear, DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = "url"
+
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
+
+
+class ActorView(DetailView):
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = "name"
+
+class FilterMoviesView(GenreYear, ListView):
+    def get_queryset(self):
+        queryset = Movie.objects.filter(year__in=self.request.GET.getlist("year"))
+        return queryset
